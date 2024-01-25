8880547cdafd026633c8058ecd130c7eddf9d9cc
Dmytro Novikov <nodm.pe@gmail.com>
2024-01-25 11:33:45 +0200

    L000: Hide secrets before deployment

--- next.config.js
+++ next.config.js
@@ -4,20 +4,12 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: 'nodm',
-        mongodb_password: 'jKtbYhaKHhbSPUOu',
-        mongodb_clustername: 'cluster0.yx6gzxd',
-        mongodb_database: 'udemy-nextjs-dev',
       },  
     };
   }
 
   return {
     env: {
-      mongodb_username: 'nodm',
-      mongodb_password: 'jKtbYhaKHhbSPUOu',
-      mongodb_clustername: 'cluster0.yx6gzxd',
-      mongodb_database: 'udemy-nextjs',
     },  
   };
 };
