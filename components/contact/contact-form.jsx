193ef54708a43c11789ca7e608591d94859c4d61
Dmytro Novikov <nodm.pe@gmail.com>
2024-01-25 09:27:14 +0200

    L282: Add contact message storing

--- components/contact/contact-form.jsx
+++ components/contact/contact-form.jsx
@@ -1,23 +1,62 @@
+import { useState } from 'react';
 import classes from './contact-form.module.css';
 
 function  ContactForm() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        name,
+        message,
+      }),
+    })
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form} action="">
+      <form className={classes.form} onSubmit={handleSubmit}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+        />
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
---
+++ pages/api/contact.js
@@ -0,0 +1,45 @@
+import Joi from 'joi';
+import { connectDatabase, insertDocument } from '../../lib/db-utils';
+
+const contactSchema = Joi.object({
+  email: Joi.string().email().required(),
+  name: Joi.string().min(3).max(20).required(),
+  message: Joi.string().min(5).max(500).required(),
+});
+
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    res.status(400).json({ message: 'Only POST method is implemented' });
+    return;
+  }
+
+  const { value: newMessage, error } = contactSchema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    const message = error.details
+      .reduce((errors, detail) => ({ ...errors, [detail.path]: detail.message }), {});
+    res.status(422).json({ message });
+    return;
+  }
+
+  let client;
+
+  try {
+    client = await connectDatabase();
+  } catch(error) {
+    res.status(500).json({ message: 'Connecting to database failed' });
+    return;
+  }
+
+  try {
+    await insertDocument(client, 'messages', newMessage);
+    res.status(201).json({ message: 'Message has been stored successfully', content: newMessage });
+  } catch(error) {
+    res.status(500).json({ message: 'Inserting data failed' });
+    return;
+  } finally {
+    client.close();
+  }
+}
+
+export default handler;
